68905e9f8eed8c70e63abc76b8f79fd3253d84a2
hamunna <hamunna@gmail.com>
2021-10-08 18:26:38 +0600

    Google Authentication Added

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+// const firebaseConfig = {
+// 	apiKey: "AIzaSyBx-qXgePlnBVRH55M1NUs2RwYFMUkdXOQ",
+// 	authDomain: "fir-authentication-form-24b3b.firebaseapp.com",
+// 	projectId: "fir-authentication-form-24b3b",
+// 	storageBucket: "fir-authentication-form-24b3b.appspot.com",
+// 	messagingSenderId: "925438930062",
+// 	appId: "1:925438930062:web:add78d7569b5f66ade6ddf"
+// };
+
+// export default firebaseConfig;
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+// import initializeAuthentication from '../Firebase/firebase.initialize';
+
+// initializeAuthentication();
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBx-qXgePlnBVRH55M1NUs2RwYFMUkdXOQ",
+  authDomain: "fir-authentication-form-24b3b.firebaseapp.com",
+  projectId: "fir-authentication-form-24b3b",
+  storageBucket: "fir-authentication-form-24b3b.appspot.com",
+  messagingSenderId: "925438930062",
+  appId: "1:925438930062:web:add78d7569b5f66ade6ddf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+
+const auth = getAuth();
 
 const Form = () => {
+	const [user, setUser] = useState('');
+
+	const handleGoogleProvider = () => {
+		signInWithPopup(auth, googleProvider)
+			.then(result => {
+				
+			});
+	}
+
 	return (
 		<div>
 			<form>
@@ -9,24 +44,24 @@ const Form = () => {
 				{/* Name Field */}
 				<div className="input-group">
 					<label htmlFor="name">Name</label>
-					<input id="name" type="text" required/>
+					<input id="name" type="text" required />
 				</div>
-				
+
 				{/* Email Field */}
 				<div className="input-group">
 					<label htmlFor="email">Email</label>
-					<input id="email" type="email" required/>
+					<input id="email" type="email" required />
 				</div>
-				
+
 				{/* Password Field */}
 				<div className="input-group">
 					<label htmlFor="password">Password</label>
-					<input id="password" type="text" required/>
+					<input id="password" type="text" required />
 				</div>
-				
+
 				{/* Checkbox Field */}
 				<div className="input-group">
-					<input id="checkbox" type="checkbox" value="login"/>
+					<input id="checkbox" type="checkbox" value="login" />
 					<label htmlFor="checkbox">Already Member?</label>
 				</div>
 
@@ -36,7 +71,7 @@ const Form = () => {
 				{/* Authentication Signup Methods */}
 				<div className="authentication-methods">
 					{/* Google Authentication */}
-					<button type="button">Google Sign In</button>
+					<button onClick={handleGoogleProvider} type="button">Google Sign In</button>
 
 					{/* GitHub Authentication */}
 					<button type="button">Github Sign In</button>
