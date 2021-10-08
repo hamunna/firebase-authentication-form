ab9ea765aac73b2e828b540ef0f2d8e6805e15d7
hamunna <hamunna@gmail.com>
2021-10-08 19:11:38 +0600

    GitHub Provider Added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,39 +1,67 @@
 import React, { useState } from 'react';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-// import initializeAuthentication from '../Firebase/firebase.initialize';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
-// initializeAuthentication();
-// Import the functions you need from the SDKs you need
+//========================================================================================
+
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBx-qXgePlnBVRH55M1NUs2RwYFMUkdXOQ",
-  authDomain: "fir-authentication-form-24b3b.firebaseapp.com",
-  projectId: "fir-authentication-form-24b3b",
-  storageBucket: "fir-authentication-form-24b3b.appspot.com",
-  messagingSenderId: "925438930062",
-  appId: "1:925438930062:web:add78d7569b5f66ade6ddf"
+	apiKey: "AIzaSyBx-qXgePlnBVRH55M1NUs2RwYFMUkdXOQ",
+	authDomain: "fir-authentication-form-24b3b.firebaseapp.com",
+	projectId: "fir-authentication-form-24b3b",
+	storageBucket: "fir-authentication-form-24b3b.appspot.com",
+	messagingSenderId: "925438930062",
+	appId: "1:925438930062:web:add78d7569b5f66ade6ddf"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const googleProvider = new GoogleAuthProvider();
+//=========================================================================================
 
+// Providers & getAuth
+const googleProvider = new GoogleAuthProvider();
+const gitHubProvider = new GithubAuthProvider();
 
 const auth = getAuth();
 
+// Component Start
 const Form = () => {
+
+	// States
 	const [user, setUser] = useState('');
 
+	//-------------------------------------------
+	// Handling Providers by onClick Handler
+	//-------------------------------------------
+
+	// Google Provider Handling
 	const handleGoogleProvider = () => {
 		signInWithPopup(auth, googleProvider)
 			.then(result => {
-				
+				const { displayName, email, photoURL } = result.user;
+
+				const loggedInUser = {
+					name: displayName,
+					email,
+					photo: photoURL
+				}
+				setUser(loggedInUser);
 			});
+	}
+
+	// GitHub Provider Hndling
+	const handleGitHubProvider = () => {
+		signInWithPopup(auth, gitHubProvider)
+			.then(result => {
+				const { displayName, email, photoURL } = result.user;
+				const loggedInUser = {
+					name: displayName,
+					email,
+					photo: photoURL
+				}
+				setUser(loggedInUser);
+			})
 	}
 
 	return (
@@ -74,13 +102,20 @@ const Form = () => {
 					<button onClick={handleGoogleProvider} type="button">Google Sign In</button>
 
 					{/* GitHub Authentication */}
-					<button type="button">Github Sign In</button>
+					<button onClick={handleGitHubProvider} type="button">Github Sign In</button>
 
 					{/* Facebook Authentication */}
 					<button type="button">Facebook Sign In</button>
 				</div>
 
 			</form>
+
+			{/* Data Display Area */}
+			<div>
+				<h3>Name: {user.name}</h3>
+				<h4>Email : {user.email}</h4>
+				<img src={user.photo} alt="" />
+			</div>
 		</div>
 	);
 };
